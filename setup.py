8cf395f8c6fd72524bac692e4e0d0696bda8aeb1
nooRok <>
2021-11-01 22:16:06 +0900

    ver.0.3.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='icr2model',
-    version='0.3.4',
+    version='0.3.5',
     packages=find_packages(exclude=['tests']),
     url='https://github.com/nooRok/icr2model',
     license='MIT',
